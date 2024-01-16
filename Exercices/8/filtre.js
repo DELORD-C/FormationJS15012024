01d3855b6944f46b49ecf5553c0840bcf1c3690d
DELORD Clément <cdelord@dawan.fr>
2024-01-16 11:58:43 +0100

    Exercices

--- Exercices/8/filtre.js
+++ Exercices/8/filtre.js
@@ -1,18 +1,20 @@
 let search = document.getElementById('search');
-let alt = alt.setAttribute('alt', '');
 
 search.addEventListener('keydown', function(event) {
+    
     if (event.key === 'Enter') {
         let keyword = search.value;
-        let elementsListe = document.getElementsByTagName('li');
-        if (keyword == '') {
+        let elementsListe = document.getElementsByTagName('img');
+        if (keyword == '') {//si la personne tape rien, on affiche tout 
             for (let element of elementsListe) {
                 element.style.display = 'block';
             }
         }
         else {
             for (let element of elementsListe) {
-                if (element.innerHTML === keyword) {
+                console.log("test")
+                let alt = element.getAttribute('alt');
+                if (alt === keyword) {
                     element.style.display = 'block';
                 }
                 else {
