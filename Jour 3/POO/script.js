f92233b6876a65a462cd889332269056fe13d2c6
DELORD Clément <cdelord@dawan.fr>
2024-01-17 11:10:53 +0100

    Vitesse MAXimale

--- Jour 3/POO/script.js
+++ Jour 3/POO/script.js
@@ -1,15 +1,17 @@
 class Vehicule {    //on crée un schema/prototype qui nous permettra de créer des objets de ce type (nb : commme scheme en arabe)
-    constructor(passager, vitesseMax, type, element, accelBtn, freinBtn) { //fonction qui se lance qd on instanciera/hydratera/creera un objet 
-        this.passager = passager;//je stocke les parametres de constructor en tant qu'attributs de la class vehicule
+    constructor(passager, vitesseMax, type, element, ABtn, FBtn, LBtn, RBtn) { //fonction qui se lance qd on instanciera/hydratera/creera un objet 
+        this.passager = passager;//je stocke les parametres de constructor en tant qu'attributs par default de la class vehicule
         this.vitesseMax = vitesseMax;
         this.type = type;
         this.vitesse = 0;
         this.element = element;
-        this.accelBtn = accelBtn;
-        this.freinBtn = freinBtn;
+        this.ABtn = ABtn;
+        this.FBtn = FBtn;
+        this.LBtn = LBtn;
+        this.RBtn = RBtn;
         this.direction = "R";
-        this.element.style.marginLeft = "0px";
-        this.element.style.marginTop = "0px";
+        this.element.style.left = "0px";
+        this.element.style.top = "0px";
     }
     
     accelerer() {
@@ -25,43 +27,99 @@ class Vehicule {    //on crée un schema/prototype qui nous permettra de créer 
     }
 
     avancer() {
-        switch (this.direction) {
+        switch (this.direction) {//on fait un cas en fonction de chaque direction
             case "R":
-                this.element.style.marginLeft = parseInt(this.element.style.marginLeft.slice(0, -2)) + this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
-                break;
+                this.element.style.left = parseInt(this.element.style.left.slice(0, -2)) + this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
+                break;//parseInt = pour transformer en nb entier 
 
             case "L":
-                this.element.style.marginLeft = parseInt(this.element.style.marginLeft.slice(0, -2)) - this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
+                this.element.style.left = parseInt(this.element.style.left.slice(0, -2)) - this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
                 break;
 
             case "U":
-                this.element.style.marginLeft = parseInt(this.element.style.marginTop.slice(0, -2)) - this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
+                this.element.style.top = parseInt(this.element.style.top.slice(0, -2)) - this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
                 break;
         
             case "D":
-                this.element.style.marginLeft = parseInt(this.element.style.marginTop.slice(0, -2)) + this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
+                this.element.style.top = parseInt(this.element.style.top.slice(0, -2)) + this.vitesse + 'px';//slice (0-2) enlève les 2 derniers caractères donc le "px"
+                break;
+        }
+    }
+
+    tournerGauche () {//on fait des cas en fonction de la position de base (ex : si on etait tt droit, on va en haut)
+        switch (this.direction) {
+            case 'R':
+                this.direction = 'U';
+                break;
+            case 'U':
+                this.direction = 'L';
+                break;
+            case 'L':
+                this.direction = 'D';
+                break;
+            case 'D':
+                this.direction = 'R';
+                break;
+        }
+    }
+
+    tournerDroite () {
+        switch (this.direction) {
+            case 'R':
+                this.direction = 'D';
+                break;
+            case 'U':
+                this.direction = 'R';
+                break;
+            case 'L':
+                this.direction = 'U';
+                break;
+            case 'D':
+                this.direction = 'L';
                 break;
         }
     }
 }
 
-//Tableau
+//pas default keypressed vaut faux
+let keyPressed = false;
+
+document.addEventListener('keydown', (event) => {//si on appuie sur une touche, keypressed prends la valeur de cette touche
+    keyPressed = event.key;
+    console.log(keyPressed);
+});
+
+document.addEventListener('keyup', (event) => {//si on relache la touche, keypressed repasse à faux
+    keyPressed = false;
+});
+
+
+
+
+//Tableau pour tous les gérer en mm temps
 let vehicules = [
-    new Vehicule(1, 5, 'velo', document.getElementById('Velo'), document.getElementById('aVelo'), document.getElementById('fVelo')),//on instancie nos éléments
-    new Vehicule(4, 150, 'Voiture', document.getElementById('Voiture'), document.getElementById('aVoiture'), document.getElementById('fVoiture'))
+    new Vehicule(1, 5, 'velo', document.getElementById('Velo'), 'z', 's', 'q', 'd'),//on instancie nos éléments
+    new Vehicule(4, 150, 'Voiture', document.getElementById('Voiture'), 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight')
 ];
 
 let boucle = setInterval(() => {
-    let pressedBtn = document.querySelector("button:active");
     vehicules.forEach(vehicule => {
-        if (pressedBtn == vehicule.accelBtn) {
-            vehicule.accelerer();
-        }
-
-        if (pressedBtn == vehicule.freinBtn) {
-            vehicule.freiner();
+        switch (keyPressed) {
+            case vehicule.ABtn://ici on verfie que keypressed = Abtn donc la bonne touche 
+                vehicule.accelerer();
+                break;
+            case vehicule.FBtn://idem...
+                vehicule.freiner();
+                break;
+            case vehicule.LBtn:
+                vehicule.tournerGauche();
+                break;
+            case vehicule.RBtn:
+                vehicule.tournerDroite();
+                break;
         }
 
         vehicule.avancer();
     });
 }, 50);
+
